16eb57afc3a514c3a3c3bdbb3c0a230c6a95f3cd
samueljarry <samueljarry.sj@gmail.com>
2022-04-23 17:05:00 +0200

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -76,15 +76,72 @@ function calculateDimensions(){
 onLoad()
 
 }
+if(window.innerWidth < 768){ // SCROLL TO VERSION MOBILE
+  
+  
+  function scrollD1() {
+    window.scrollTo({
+      top: 3.6 * window.innerHeight,
+      right: 0
+    });
+  }
+  
+  function scrollC1() {
+    window.scrollTo({
+      top: 5.6 * window.innerHeight,
+      right: 0
+    });
+  }
+  }
 
-function show() {
-    document.getElementById('test').classList.toggle('active');
-    }
+if(window.innerWidth > 768){ // SCROLL TO VERSION DESKTOP
+function scrollA() {
+  window.scrollTo({
+    top: 0,
+    right: 0,
+    behavior: "smooth",
+    easing : "ease-in"
+  });
+}
 
-    function showDiv() {
-        document.getElementById('site1').classList.toggle('active');
-        document.getElementById('site1').style.display = "block";
-        document.getElementById("site1").animate([
+function scrollD() {
+  window.scrollTo({
+    top: 1.78 * window.innerWidth,
+    right: 0,
+    behavior: "smooth",
+    easing : "ease-in"
+  });
+}
+
+function scrollD1() {
+  window.scrollTo({
+    top: 1.78 * window.innerWidth,
+    right: 0
+  });
+}
+
+function scrollC() {
+  window.scrollTo({
+    top: 2.79 * window.innerWidth,
+    right: 0,
+    behavior: "smooth",
+    easing : "ease-in-out"
+  });
+}
+
+function scrollC1() {
+  window.scrollTo({
+    top: 2.79 * window.innerWidth,
+    right: 0
+  });
+}
+}
+
+
+    function showSource() {
+        document.getElementById('lasource').classList.toggle('active');
+        document.getElementById('lasource').style.display = "block";
+        document.getElementById("lasource").animate([
             // étapes/keyframes
             { transform: 'translateY(-100vh)' },
             { transform: 'translateY(0vh)' }
@@ -97,9 +154,9 @@ function show() {
           
      }
 
-     function closeDiv() {
-        document.getElementById('site1').classList.toggle('disabled');
-        document.getElementById("site1").animate([
+     function closeSource() {
+        document.getElementById('lasource').classList.toggle('disabled');
+        document.getElementById("lasource").animate([
             // étapes/keyframes
             { transform: 'translateY(0vh)' },
             { transform: 'translateY(-100vh)' }
@@ -109,25 +166,178 @@ function show() {
             fill: "forwards",
             easing: "ease-in-out"
           });
+}
+
+function showEclo() {
+    document.getElementById('eclothing').classList.toggle('active');
+    document.getElementById('eclothing').style.display = "block";
+    document.getElementById("eclothing").animate([
+        // étapes/keyframes
+        { transform: 'translateY(-100vh)' },
+        { transform: 'translateY(0vh)' }
+      ], {
+        // temporisation
+        duration: 450,
+        fill: "forwards",
+        easing: "ease-in-out"
+      });
+      
+ }
+
+ function closeEclo() {
+    document.getElementById('eclothing').classList.toggle('disabled');
+    document.getElementById("eclothing").animate([
+        // étapes/keyframes
+        { transform: 'translateY(0vh)' },
+        { transform: 'translateY(-100vh)' }
+      ], {
+        // temporisation
+        duration: 450,
+        fill: "forwards",
+        easing: "ease-in-out"
+      });
+}
+
+function showPort() {
+  document.getElementById('portfolio').classList.toggle('active');
+  document.getElementById('portfolio').style.display = "block";
+  document.getElementById("portfolio").animate([
+      // étapes/keyframes
+      { transform: 'translateY(-100vh)' },
+      { transform: 'translateY(0vh)' }
+    ], {
+      // temporisation
+      duration: 450,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
+}
+
+function closePort() {
+  document.getElementById('portfolio').classList.toggle('disabled');
+  document.getElementById("portfolio").animate([
+      // étapes/keyframes
+      { transform: 'translateY(0vh)' },
+      { transform: 'translateY(-100vh)' }
+    ], {
+      // temporisation
+      duration: 450,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
+}
+
+function showThm() {
+  document.getElementById('thm').classList.toggle('active');
+  document.getElementById('thm').style.display = "block";
+  document.getElementById("thm").animate([
+      // étapes/keyframes
+      { transform: 'translateY(-100vh)' },
+      { transform: 'translateY(0vh)' }
+    ], {
+      // temporisation
+      duration: 450,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
+    
+}
+
+function closeThm() {
+  document.getElementById('thm').classList.toggle('disabled');
+  document.getElementById("thm").animate([
+      // étapes/keyframes
+      { transform: 'translateY(0vh)' },
+      { transform: 'translateY(-100vh)' }
+    ], {
+      // temporisation
+      duration: 450,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
 
 }
 
+function showCrypto() {
+  document.getElementById('crypto').classList.toggle('active');
+  document.getElementById('crypto').style.display = "block";
+  document.getElementById("crypto").animate([
+      // étapes/keyframes
+      { transform: 'translateY(-100vh)' },
+      { transform: 'translateY(0vh)' }
+    ], {
+      // temporisation
+      duration: 450,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
+}
+
+function closeCrypto() {
+  document.getElementById('crypto').classList.toggle('disabled');
+  document.getElementById("crypto").animate([
+      // étapes/keyframes
+      { transform: 'translateY(0vh)' },
+      { transform: 'translateY(-100vh)' }
+    ], {
+      // temporisation
+      duration: 450,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
+}
+
+function showKey() {
+  document.getElementById('keylogger').classList.toggle('active');
+  document.getElementById('keylogger').style.display = "block";
+  document.getElementById("keylogger").animate([
+      // étapes/keyframes
+      { transform: 'translateY(-100vh)' },
+      { transform: 'translateY(0vh)' }
+    ], {
+      // temporisation
+      duration: 450,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
+}
+
+function closeKey() {
+  document.getElementById('keylogger').classList.toggle('disabled');
+  document.getElementById("keylogger").animate([
+      // étapes/keyframes
+      { transform: 'translateY(0vh)' },
+      { transform: 'translateY(-100vh)' }
+    ], {
+      // temporisation
+      duration: 450,
+      fill: "forwards",
+      easing: "ease-in-out"
+    });
+}
 // NAVBAR COLOR
 
 let Hauteur = window.innerHeight;
 let Largeur = 0.97 * window.innerWidth;
 let Largeur1 = 1.77 * window.innerWidth;
+let Largeur2 = 2.77 * window.innerWidth;
+let Largeur3 = 3.77 * window.innerWidth;
 
 // console.log(viewportHeight, Largeur);
 
 window.onscroll = function() {
     var nav = document.getElementById('sidebar');
-    if ( window.pageYOffset > Largeur ) {
+    if ( window.pageYOffset > Largeur) {
         nav.classList.add("scroll");
     } else {
         nav.classList.remove("scroll");
     } if ( window.pageYOffset > Largeur1 ) {
         nav.classList.remove("scroll");
+} if ( window.pageYOffset > Largeur2) {
+  nav.classList.add("scroll1");
+} else {
+  nav.classList.remove('scroll1');
+} if ( window.pageYOffset > Largeur3) {
+  nav.classList.remove('scroll1')
 }
 }
-
